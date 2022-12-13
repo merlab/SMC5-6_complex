217be44b5a48c9e271c8593ac774e1b790344f2a
ArvinZaker <arv.zaker@gmail.com>
2022-12-13 13:08:47 -0500

    finalized upset plot

--- R/fig1_upset.R
+++ R/fig1_upset.R
@@ -1,51 +1,49 @@
 # https://krassowski.github.io/complex-upset/articles/Examples_R.html
 source("./R/routine_tasks.R")
 library(ComplexHeatmap)
-cbpd <- readRDS('./results/cbioportal_alt_all.rds')
+cbpd <- readRDS("./data/cbioportal_data.rds")
 cbpd <- cbpd[cbpd$isalt == 1, ]
 cbpd <- cbpd[cbpd$major != 'Other',]
 cbpd$major <- factor(cbpd$major)
 
 makeUpset <- function(sdf, title, cut = 2) {
-    colors <- c('#66c2a5','#fc8d62','#8da0cb')
     m <- make_comb_mat(sdf, mode = 'distinct')
+    # we remove any combination under 2 samples
     m <- m[comb_size(m) >= cut]
-    print(m)
     # data at the top of teh upset plot
-    breaks <- c(1, 3, 10, 20, 50, 100, 250, 500, 1000, 2000)
+    breaks <- c(1, 10, 100, 1000, 2000)
+    # top annotation bar plot
     top_ha <- HeatmapAnnotation(
         'Number of \n patients' = anno_barplot(
                 log10(comb_size(m))
                 , bar_width = 0.8
-                , gp = gpar(fill = 'black')#colors[1])#"black")
+                , gp = gpar(fill = '#3C3D41', color = '#3C3D41', fontface = 'bold')
                 , border = FALSE
                 , height = unit(5, "cm")
                 , ylim = c(1,log10(2000))
-                , axis_param = list(at = log10(breaks), labels = breaks)
-                                )
+                , axis_param = list(at = log10(breaks), labels = breaks))
         , annotation_name_side = "left"
         , annotation_name_rot = 0
     )
-    # data at the bottom of the upset plot
+    # right annotation bar plot
     right_ha = rowAnnotation(
         'Set size' = anno_barplot(
                 log10(set_size(m))
                 , bar_width = 0.8
-                , gp = gpar(fill = 'black')#colors[3])#"black")
+                , gp = gpar(fill = '#3C3D41', color = '#3C3D41', fontface = 'bold')
                 , border = FALSE
                 , width = unit(5, "cm")
                 , ylim = c(1, log10(2000))
-                , axis_param = (list(at = log10(breaks), labels = breaks))
+                , axis_param = list(at = log10(breaks), labels = breaks)
                 )
         , annotation_name_side = "bottom"
         )
+    # main upset plot
     UpSet(m, comb_order = rev(order(comb_size(m)))
         , top_annotation = top_ha
         , row_names_side = 'left'
         , left_annotation = right_ha
         )
-
-
 }
 
 pdf('./figures/fig1_upset.pdf', onefile = TRUE, width = 8, height = 5)
