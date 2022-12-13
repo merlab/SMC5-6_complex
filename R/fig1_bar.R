eb6f4b470b3c38036a946fcfe4be31fe79d050f6
ArvinZaker <arv.zaker@gmail.com>
2022-12-13 12:58:47 -0500

    finalized fig1 bar chart

--- R/fig1_bar.R
+++ R/fig1_bar.R
@@ -1,11 +1,11 @@
 # config
 ylim <- 50
-#program
+# load libs and functions
 library(ggplot2)
-library(ggpubr)
-source('./R/routine_tasks.R')
 
+# load data
 df <- readRDS("./data/cbioportal_data.rds")
+# exclude others
 df <- df[df$major != "Other", ]
 
 # find frequency of mutation for each cancer type
@@ -15,31 +15,41 @@ for(i in unique(df$major)) {
     mut <- sum(as.numeric(df$isalt[df$major == i]))
     altprop <- mut / tot * 100
     plot_df <- rbind(plot_df
-      , c(x = paste0(i, '\n', '(n = ', tot, ')'), y = altprop, group = 'A')
+      , c(x = i, y = altprop
+      , label = paste0(round(altprop,digits = 1), ' % (n=', tot, ')')
+      , group = 'A')
     )
 }
-colnames(plot_df) <- c('x', 'y', 'group')
+colnames(plot_df) <- c('x', 'y', 'label','group')
 plot_df$y <- as.numeric(plot_df$y)
-plot_df <- plot_df[plot_df$x != 'Others', ]
 
 plot_df$blank <- ylim
 
 # https://community.rstudio.com/t/how-to-change-the-order-in-stacked-bar-chart/82093/2
 # bar chart ggplot
 barchart <- ggplot(plot_df, aes(x=reorder(x, y), y = y, fill = group)) + 
+  # add blank grey bar
   geom_bar(data = plot_df, mapping = aes(x = reorder(x,y), y = blank)
-         , fill = 'grey80', stat = 'identity') +
+         , fill = "#CCCCCC", stat = 'identity') +
+  # bar chart
   geom_bar(stat = "identity") +
-  geom_text(aes(label = paste0(round(plot_df$y, digits = 1), '%')), hjust = -0.2, color = "black") + 
+  # text on the bar chart
+  geom_text(aes(label = label), hjust = -0.1, color = "#3C3D41", size = 4) +
+  # axis labeling
   xlab('') +
   ylab('% of samples with alteration') +
-  scale_fill_manual(values = c("black", "grey50")) + # '#66c2a5'
+  # coloring
+  scale_fill_manual(values = c("#3C3D41", "#CCCCCC")) +
   ylim(0 , ylim) +
   # NOTE: expand makes the start to be flush to left
   scale_y_continuous(expand = c(0,0), limits = c(0,ylim), position="right") +
-  theme_classic() + 
+  # themeing
+  theme_classic() +
   coord_flip() +
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        text = element_text(face = 'bold'),
+        axis.text.y = element_text(size = 8, color = 'black'))
+# plotting
 pdf('./figures/fig1_bar.pdf', height = 5, width = 4)
 plot(barchart)
 dev.off()
