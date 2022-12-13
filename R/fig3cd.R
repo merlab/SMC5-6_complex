946cf61d17dea7d5c0a92fff6454031c123a9629
ArvinZaker <arv.zaker@gmail.com>
2022-12-13 17:01:53 -0500

    added fig3

--- R/fig3cd.R
+++ R/fig3cd.R
@@ -1,22 +1,30 @@
+###################
+## Configuration ##
+###################
+studies <- c('Breast Cancer (METABRIC, Nature 2012 & Nat Commun 2016)')
+# columns used for analysis
+cols <- c(isalt = "Complex", NSMCE2 = "NSMCE2")
+# color of the plot
+ref_df <- readRDS("./data/cbioportal_data.rds")
 # code taken from:
 # https://stackoverflow.com/questions/3483203/create-a-boxplot-in-r-that-labels-a-box-with-the-sample-size-n
 # rain cloud plots from
 # https://github.com/RainCloudPlots/RainCloudPlots
+# libs
 library(ComplexHeatmap)
 library(ggplot2)
-library(ggpubr)
 library(survminer)
 library(survival)
-library(dplyr)
-library(cowplot)
-source('./R/R_rainclouds.R')
-dir <- getwd()
-genes <- c("NSMCE2", "Complex")
-tissue <- c("Breast")
-colors <- c("#CB3814", "#3361BD")
 
-KM_survival_plot <- function(sur_df, colors, title, xlab = TRUE, ylab = TRUE, strata = FALSE) {
+KM_survival_plot <- function(sur_df, title, xlab = TRUE, ylab = TRUE, strata = FALSE, censor = TRUE) {
+  colors <- c('#DE3B1C','#707176')
   gc()
+  #censoring function
+  if(censor == TRUE) {
+    sur_df$OVS[sur_df$OVT > 60] <- 0
+    sur_df$OVT[sur_df$OVT > 60] <- 60 
+  }
+
   diff <- survdiff(Surv(OVT, OVS)~ group, data = sur_df)
   p.val <- 1 - pchisq(diff$chisq, length(diff$n) - 1)
 
@@ -34,12 +42,11 @@ KM_survival_plot <- function(sur_df, colors, title, xlab = TRUE, ylab = TRUE, st
         , ylab = ifelse(ylab, "Probability of overall survival",'')
         , title = title
         , legend.title='Status'
-        , legend = c(.85,.8)
+        , legend = c(.85,.4)
         , legend.labs = c('Altered', 'Wild')
         , risk.table = TRUE
-        , risk.table.y.text.col = TRUE
+        , axes.offset = FALSE
         , risk.table.height = 0.22
-        , xlim = c(0, 360)
   ) 
   ggsurv$table <- ggrisktable(fit
         , data = sur_df
@@ -52,13 +59,13 @@ KM_survival_plot <- function(sur_df, colors, title, xlab = TRUE, ylab = TRUE, st
         , axes.offset = TRUE
         , tables.theme = theme_classic()
         , fontsize = 3.25
-        , xlim = c(0, 360)
         , risk.table.col = "strata"
         # NEEDED TO GET THE RISK TABLE CORRECTLY
-        , break.time.by = 50
+        , break.time.by = ifelse(censor,10,50)
   ) + scale_y_discrete(labels = c('Wild', 'Altered')) +
       theme(axis.text.x = element_text(size = 12),
-            axis.title.x = element_text(size = 12),
+            axis.title.x = element_text(size = 12, face = 'bold'),
+            axis.title.y = element_text(size = 12, face = 'bold'),
             legend.text = element_blank(),
             legend.title = element_blank())
   ggsurv$plot <- ggsurv$plot + theme(plot.title = element_text(hjust = 0.5, size = 12)) 
@@ -66,41 +73,42 @@ KM_survival_plot <- function(sur_df, colors, title, xlab = TRUE, ylab = TRUE, st
   ggsurv$plot <- ggsurv$plot + theme(plot.title = element_text(hjust = 0.5, size = 12)) 
   ggsurv$plot <- ggsurv$plot + theme(plot.margin = unit(c(5, 5, 0, 5), "points"))
   ggsurv$table <- ggsurv$table + theme(plot.margin = unit(c(5, 5, 0, 5), "points"))
-  ggsurv$plot <- ggsurv$plot + scale_x_continuous(limits = c(0,360), breaks = seq(0, 350, 50))
-  #ggsurv$table <- ggsurv$table + scale_x_continuous(limits = c(0,360), breaks = seq(0, 300, 100))
+  if(censor == TRUE) {
+    ggsurv$plot <- ggsurv$plot + scale_x_continuous(limits = c(0,65), breaks = seq(0, 60, 10))
+    ggsurv$table <- ggsurv$table + scale_x_continuous(limits = c(0,65), breaks = seq(0, 60, 10))
+  } else {
+    ggsurv$table <- ggsurv$table + scale_x_continuous(limits = c(0,360), breaks = seq(0, 350, 50))
+    ggsurv$plot <- ggsurv$plot + scale_x_continuous(limits = c(0,360), breaks = seq(0, 350, 50))
+  }
+  if(signif(p.val,1) == 1e-6) {
+    print(p.val)
+    p.val_text <- bquote("p = " ~ '1' ~ 'x' ~ 10^-6)
+  }
+  if(signif(p.val,1) == 5e-4) {
+    print(p.val)
+    p.val_text <- bquote("p = " ~ '5' ~ 'x' ~ 10^-4)
+  }
   if(signif(p.val,1) == 1e-5) {
     print(p.val)
-    p.val_text <- bquote("P = " ~ '1' ~ 'x' ~ 10^-5)
+    p.val_text <- bquote("p = " ~ '1' ~ 'x' ~ 10^-5)
   }
   if(signif(p.val,1) == 6e-4) {
     print(p.val)
-    p.val_text <- bquote("P = " ~ '6' ~ 'x' ~ 10^-4)
+    p.val_text <- bquote("p = " ~ '6' ~ 'x' ~ 10^-4)
   }
+
   ggsurv$plot <- ggsurv$plot + annotate(
-      geom="text", x=50, y=0.1,
-              color="black", size = 5,
-              label=p.val_text
+      geom="text", x=ifelse(censor,10,50), y=0.1,
+              color="black", size = 3,
+              label=p.val_text, face = 'plain'
               )
-  #ggsurv$plot <- ggsurv$plot + annotate(
-  #    geom="text", x=50, y=0.1,
-  #            color="black", size = 5,
-  #            label=signif(p.val,3)
-  #            )
   # this is to bring risk table up
   rm(fit, sur_df)
   fit <- NA
   return(ggsurv)
 }
 
-###################
-## Configuration ##
-###################
-studies <- c('Breast Cancer (METABRIC, Nature 2012 & Nat Commun 2016)'
-    )
-# columns used for analysis
-cols <- c(isalt = "Complex", NSMCE2 = "NSMCE2")
-# color of the plot
-ref_df <- as.data.frame(readRDS(sprintf("./results/cbioportal_alt_all.rds")))
+
 
 sur_dfs <- list()
 titles <- list()
@@ -125,9 +133,20 @@ for (current_study in studies) {
       }
 }
 
-pdf("./figures/fig3row2NC.pdf", width = 10, height = 5)
-p <- list((KM_survival_plot(sur_dfs[[1]], color = colors, title = titles[[1]]))
-            , (KM_survival_plot(sur_dfs[[2]], color = colors, title = titles[[2]])))
+
+pdf("./figures/fig3cd_censored.pdf", width = 10, height = 5)
+p <- list((KM_survival_plot(sur_dfs[[1]], title = "SMC5/6 complex", censor = TRUE))
+            , (KM_survival_plot(sur_dfs[[2]], title = "NSMCE2", censor = TRUE)))
+arrange_ggsurvplots(p,
+  print = TRUE,
+  ncol = 2,
+  nrow = 1
+)
+dev.off()
+
+pdf("./figures/fig3cd_notcensored.pdf", width = 10, height = 5)
+p <- list((KM_survival_plot(sur_dfs[[1]], title = "SMC5/6 complex", censor = FALSE))
+            , (KM_survival_plot(sur_dfs[[2]], title = "NSMCE2", censor = FALSE)))
 arrange_ggsurvplots(p,
   print = TRUE,
   ncol = 2,
